69047814f9dc25936a4ed5fc2f79dacdd32ddf6a
João Bonifácio <joaojunior.b22@gmail.com>
2023-11-16 14:23:19 -0300

    passwordHash-implements

--- backend/src/user/users.controller.ts
+++ backend/src/user/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from 'src/user/users.service';
 
 @Controller('user')
--- backend/src/user/users.service.ts
+++ backend/src/user/users.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/Prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
   autenticate() {
     //verificar jwt
     return { message: 'ok' };
   }
+
   async joinUser(body) {
+    const { name, email, password } = body;
+    const salt = 12;
+    const hash = await bcrypt.hash(password, salt);
+
     await this.prisma.userModel.create({
-      data: {
-        ...body,
-        staff: 'user',
-      },
+      data: { name, email, password: hash, staff: 'user' },
     });
     return { message: 'cadastrado com sucesso' };
   }
