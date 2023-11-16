a0aefbbeda3ff6281be269a000c9ba2a9de1735f
João Bonifácio <joaojunior.b22@gmail.com>
2023-11-16 18:49:06 -0300

    Upgrade-sanatizing-input

--- backend/src/DTO/userDTO.ts
+++ backend/src/DTO/userDTO.ts
@@ -7,7 +7,21 @@ export const userLoginZ = z
   })
   .refine((data) => Object.keys(data).length === 2, {
     message: 'All fields (name, email, password) are required',
-  });
+  })
+  .refine(
+    (data) => {
+      const allowedFields = ['name', 'password'];
+      const receivedFields = Object.keys(data);
+      const invalidFields = receivedFields.filter(
+        (field) => !allowedFields.includes(field),
+      );
+      if (invalidFields.length > 0) {
+        throw new Error(`Invalid fields: ${invalidFields.join(', ')}`);
+      }
+      return true;
+    },
+    { message: 'Invalid fields provided' },
+  );
 
 export const userJoinZ = z
   .object({
@@ -17,4 +31,18 @@ export const userJoinZ = z
   })
   .refine((data) => Object.keys(data).length === 3, {
     message: 'All fields (name, email, password) are required',
-  });
+  })
+  .refine(
+    (data) => {
+      const allowedFields = ['name', 'email', 'password'];
+      const receivedFields = Object.keys(data);
+      const invalidFields = receivedFields.filter(
+        (field) => !allowedFields.includes(field),
+      );
+      if (invalidFields.length > 0) {
+        throw new Error(`Invalid fields: ${invalidFields.join(', ')}`);
+      }
+      return true;
+    },
+    { message: 'Invalid fields provided' },
+  );
