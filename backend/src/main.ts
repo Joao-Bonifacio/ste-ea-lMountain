2658bdb20b3438293455a62114c38fd19205e58c
João Bonifácio <joaojunior.b22@gmail.com>
2023-11-17 19:40:23 -0300

    fix-ports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 const options: CorsOptions = {
   origin: [
-    'http://172.29.45.36:3001',
-    'http://localhost:3001',
-    'http://129.168.1.115:3001',
+    'http://172.29.45.36:3000',
+    'http://localhost:3000',
+    'http://129.168.1.115:3000',
   ],
   credentials: true,
 };
